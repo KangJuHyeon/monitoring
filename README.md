f78763e7b23cd5add83f66a1f29524567fd74a39
KANGJUHYEON <alinos@naver.com>
2021-10-13 01:37:28 +0900

    login

--- README.md
+++ README.md
@@ -4,4 +4,3 @@ Welcome to the monitoring wiki!
 ```
 다른 서버에 ping을 보낸 후에 서버가 정상 작동하는지 안 하는지를 확인하고 웹에서 데이터를 모니터링할 수 있게 만들 예정입니다.
 ```
-<br/><br/>
--- controllers/users/login.js
+++ controllers/users/login.js
@@ -1,3 +1,20 @@
-module.exports = (req, res) => {
-    res.send("HelloLogin");
+const { user } = require('../../models');
+
+module.exports = async (req, res) => {
+    const { nickname, password, retrypassword } = req.body;
+
+    try{
+        const userInfo = await user.create({
+            nickname,
+            password,
+            retrypassword
+        });
+    
+        res.status(200).send({ 
+            userInfo,
+            message : "성공적으로 로그인되었습니다." 
+        });
+    }catch(error){
+        console.error(error);
+    }
 }

6863455fa0b80f6f82c4ca0ad1aa73bc6186e098
KANGJUHYEON <alinos@naver.com>
2021-11-04 23:15:24 +0900

    .gitignore 다시 적용

--- config/config.js
+++ config/config.js
@@ -1,27 +1,26 @@
 const dotenv = require('dotenv');
-
 dotenv.config();
 
 module.exports = {
-  development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DATABASE,
-    host: '127.0.0.1',
-    dialect: 'mysql',
-  },
-  test: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DATABASE,
-    host: '127.0.0.1',
-    dialect: 'mysql',
-  },
-  production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DATABASE,
-    host: '127.0.0.1',
-    dialect: 'mysql',
-  },
+    development: {
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DATABASE,
+        host: '127.0.0.1',
+        dialect: 'mysql',
+    },
+    test: {
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DATABASE,
+        host: '127.0.0.1',
+        dialect: 'mysql',
+    },
+    production: {
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DATABASE,
+        host: '127.0.0.1',
+        dialect: 'mysql',
+    },
 };

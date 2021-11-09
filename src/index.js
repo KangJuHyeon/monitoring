28138345b9f435a8ef1c99fc1a62ae10334e135d
KANGJUHYEON <alinos@naver.com>
2021-11-10 01:00:23 +0900

    redux-persist add

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 import { createStore } from 'redux';
 import rootReducer from './reducers/index';
 import { Provider } from 'react-redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 const store = createStore(rootReducer, composeWithDevTools());
+const persistor = persistStore(store);
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+            <App />
+        </PersistGate>
     </Provider>,
     document.getElementById('root'),
 );
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,16 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
 import userReducer from './userReducer';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+    // localStorage에 저장합니다.
+    key: 'root',
+    storage,
+};
 
 const rootReducer = combineReducers({
     userReducer,
 });
 
-export default rootReducer;
+export default persistReducer(persistConfig, rootReducer);
